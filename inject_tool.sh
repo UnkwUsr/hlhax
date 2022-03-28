0d1006893559570e2c676d7ad1c2201f4f72dcfc
UnkwUsr <ktoto2707043@gmail.com>
2022-03-28 23:36:41 +0300

    [injector] Do not restore on crash when debug

--- inject_tool.sh
+++ inject_tool.sh
@@ -19,10 +19,12 @@ fi
 libpath=$(realpath ./libhlhack.so)
 #how its work: if we call lldb with '--batch', then lldb will exit after done script, else they dont exit, and just wait user(what we need for debug)
 do_batch="--batch"
+do_restore_on_crash="-u 1 --"
 for param in "$@"
 do
     if [ ${param:0:1} = "d" ]; then
         do_batch=""
+        do_restore_on_crash="-u 0 --"
 
         param=${param:1}
     fi
@@ -33,7 +35,7 @@ do
             exit 4
         fi
         sudo lldb $do_batch --attach-pid $pid \
-            --one-line "exp (void*)dlopen(\"$libpath\", 0x00002)" \
+            --one-line "exp $do_restore_on_crash auto \$handle = (void*)dlopen(\"$libpath\", 0x00002)" \
             --one-line 'p (char*)dlerror()'
         echo "Loaded!"
         ;;
@@ -43,17 +45,17 @@ do
             exit 5
         fi
         sudo lldb $do_batch --attach-pid $pid \
-            --one-line "exp auto \$lib_addr_loaded = (void*(*)(char*, int)) dlopen(\"$libpath\", 6)" \
-            --one-line "exp (int(*)(void*)) dlclose(\$lib_addr_loaded)" \
-            --one-line "exp (int(*)(void*)) dlclose(\$lib_addr_loaded)"
+            --one-line "exp  auto \$handle = (void*(*)(char*, int)) dlopen(\"$libpath\", 6)" \
+            --one-line "exp (int(*)(void*)) dlclose(\$handle)" \
+            --one-line "exp $do_restore_on_crash (int(*)(void*)) dlclose(\$handle)"
         echo "UnLoaded!"
         ;;
     reload)
         if grep -q $libpath /proc/$pid/maps; then
             sudo lldb $do_batch --attach-pid $pid \
-                --one-line "exp auto \$lib_addr_loaded = (void*(*)(char*, int)) dlopen(\"$libpath\", 6)" \
-                --one-line "exp (int(*)(void*)) dlclose(\$lib_addr_loaded)" \
-                --one-line "exp (int(*)(void*)) dlclose(\$lib_addr_loaded)" \
+                --one-line "exp auto \$handle = (void*(*)(char*, int)) dlopen(\"$libpath\", 6)" \
+                --one-line "exp (int(*)(void*)) dlclose(\$handle)" \
+                --one-line "exp (int(*)(void*)) dlclose(\$handle)" \
                 --one-line "exp (void*)dlopen(\"$libpath\", 0x00002)" \
                 --one-line 'p (char*)dlerror()'
         else
