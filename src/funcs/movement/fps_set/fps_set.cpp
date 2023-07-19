5f0ab031ab58f21f158dffa9f0537c8559df4702
UnkwUsr <ktoto2707043@gmail.com>
2023-07-19 22:06:34 +0300

    [fps_set] feat: add cvars to exec cmds on on/off

--- src/funcs/movement/fps_set/fps_set.cpp
+++ src/funcs/movement/fps_set/fps_set.cpp
@@ -7,6 +7,8 @@
 
 namespace Cvars {
     cvar_t* fps_critical;
+    cvar_t* fps_boost_cmd_on;
+    cvar_t* fps_boost_cmd_off;
 }
 
 namespace FpsSet {
@@ -18,11 +20,32 @@ namespace FpsSet {
     {
         ADD_HOOK(CL_CreateMove, gp_Client)
 
-        // Note: before use fps_set keep sure that fps_max is much bigger than fps_critical(just set something like 100000)
+        // Note: before using fps_set keep sure that fps_max is much higher
+        // than fps_critical (but do not set fps_max very high!!! Best value is
+        // just 1000, as higher it goes, slower you actually will be. Probably.
+        // Read more at: https://wiki.sourceruns.org/wiki/FPS_Effects)
 
         CREATE_TGL_CMD("fps_set", fps_set_on, fps_set_off)
 
         Cvars::fps_critical = CREATE_CVAR("fps_critical", "500");
+        Cvars::fps_boost_cmd_on = CREATE_CVAR("fps_boost_cmd_on", "\
+                hud_draw 0;\
+                r_drawentities 0;\
+                r_drawviewmodel 0;\
+                cv_esp 0;\
+                cv_tracer 0;\
+                cv_barrel 0;\
+                fps_max 1000;\
+                fps_override 1;");
+        Cvars::fps_boost_cmd_off = CREATE_CVAR("fps_boost_cmd_off", "\
+                hud_draw 1;\
+                r_drawentities 1;\
+                r_drawviewmodel 1;\
+                cv_esp 1;\
+                cv_tracer 1;\
+                cv_barrel 1;\
+                fps_max 100;");
+
     }
 
 
@@ -60,13 +83,16 @@ namespace FpsSet {
     void fps_set_on()
     {
         b_fps_set = true;
+
+        gp_Engine->pfnClientCmd(Cvars::fps_boost_cmd_on->string);
     }
     void fps_set_off()
     {
         b_fps_set = false;
-
         gp_Engine->Cvar_SetValue("r_norefresh", 0);
         gp_Engine->Cvar_SetValue("cl_showfps", 1);
+
+        gp_Engine->pfnClientCmd(Cvars::fps_boost_cmd_off->string);
     }
 
 
